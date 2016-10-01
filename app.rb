fd43fafe3c8999642004beea3bdd53570eb8ac44
Jon Moss <me@jonathanmoss.me>
2016-10-01 19:15:09 -0400

    Create a stub /gpa endpoint

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
 
+require_relative 'lib/grade_fetcher'
+
 module PowerGPA
   class Application < ::Sinatra::Base
     get '/' do
       erb :index
+    end
+
+    get '/gpa' do
+      content_type :json
+      JSON.dump GradeFetcher.new(params).to_h
     end
   end
 end
---
+++ lib/grade_fetcher.rb
@@ -0,0 +1,11 @@
+module PowerGPA
+  class GradeFetcher
+    def initialize(params)
+      @params = params
+    end
+
+    def to_h
+      {}
+    end
+  end
+end
