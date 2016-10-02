2fa6aad3ca6123248468f489edf4321682395067
Jon Moss <me@jonathanmoss.me>
2016-10-01 20:11:34 -0400

    Create GPACalculator to calculate a GPA based on grades hash

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 require_relative 'lib/grade_fetcher'
+require_relative 'lib/gpa_calculator'
 
 module PowerGPA
   class Application < ::Sinatra::Base
@@ -9,8 +10,14 @@ module PowerGPA
     end
 
     get '/gpa' do
+      grades = GradeFetcher.new(params).to_h
+      gpa = GPACalculator.new(grades).to_h
+
       content_type :json
-      JSON.dump GradeFetcher.new(params).to_h
+      JSON.dump({
+        grades: grades,
+        gpa: gpa
+      })
     end
   end
 end
---
+++ lib/gpa_calculator.rb
@@ -0,0 +1,56 @@
+module PowerGPA
+  class GPACalculator
+    def initialize(grades)
+      @grades = grades
+    end
+
+    def to_h
+      # first convert based on unweighter GPA scale
+      @grades.each do |course, grade|
+        @grades[course] =
+          if grade >= 92.5
+            4.0
+          elsif grade >= 89.5 && grade < 92.5
+            3 + Rational(2, 3).to_f
+          elsif grade >= 86.5 && grade < 89.5
+            3 + Rational(1, 3).to_f
+          elsif grade >= 82.5 && grade < 86.5
+            3.0
+          elsif grade >= 79.5 && grade < 82.5
+            2 + Rational(2, 3).to_f
+          elsif grade >= 76.5 && grade < 79.5
+            2 + Rational(1, 3).to_f
+          elsif grade >= 72.5 && grade < 76.5
+            2
+          elsif grade >= 69.5 && grade < 72.5
+            1 + Rational(2, 3).to_f
+          elsif grade >= 66.5 && grade < 69.5
+            1 + Rational(1, 3).to_f
+          elsif grade >= 64.5 && grade < 66.5
+            1
+          else
+            0
+          end
+      end
+
+      # next convert to weighted scale
+      @grades.each do |course, grade|
+        @grades[course] =
+          if course.start_with?('AP ')
+            grade + Rational(2, 3).to_f
+          elsif course.end_with?(' Acc')
+            grade + Rational(1, 3).to_f
+          elsif course.end_with?(' CPA')
+            # no-op
+            grade
+          end
+      end
+
+      # add up the weighted numbers, and divide by the number of classes being taken
+      grades_sum = @grades.values.inject(0) { |sum, x| sum + x }
+
+      # return the sum divided by the number of classes!
+      (grades_sum / @grades.keys.size)
+    end
+  end
+end
