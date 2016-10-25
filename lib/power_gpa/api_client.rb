c7b1f8ef78bca200b078475dd9e6619b01d4433f
Jon Moss <me@jonathanmoss.me>
2016-10-25 17:07:12 -0400

    Add collapsable accordions in preparation for having multiple school years be present on the /gpa page
    This is a transitional UI, and is laying the foundation for when
    multiple school years will be displayed on `/gpa`.

--- lib/power_gpa/api_client.rb
+++ lib/power_gpa/api_client.rb
@@ -120,7 +120,10 @@ module PowerGPA
             end
           end
 
-          return_data[d['student']['firstName']] = final_grades
+          return_data[d['student']['firstName']] ||= {}
+          return_data[d['student']['firstName']][d['student']['gradeLevel']] = final_grades
+
+          #return_data[d['student']['firstName']] = final_grades
         end
 
         return_data
