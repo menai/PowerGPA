8b499961ac234d370641f0d1ce480e09ac1cb136
Jon Moss <me@jonathanmoss.me>
2016-10-28 17:10:18 -0400

    Move PowerGPA::APIClient::Student to a separate file

--- lib/power_gpa/api_client.rb
+++ lib/power_gpa/api_client.rb
@@ -2,6 +2,7 @@ require 'savon'
 require 'json'
 require 'uri'
 require 'power_gpa/rollbar_reporter'
+require 'power_gpa/api_client/student'
 
 module PowerGPA
   class APIClient
@@ -73,124 +74,6 @@ module PowerGPA
         true
       rescue SocketError
         false
-      end
-    end
-
-    class Student
-      ACC_COURSE_WHITELIST = {
-        'Science Research 1' => 'Science Research 1 Acc'
-      }
-
-      AP_COURSE_WHITELIST = {
-        'Science Research 2' => 'AP Science Research 2',
-        'Science Research 3' => 'AP Science Research 3'
-      }
-
-      def initialize(client, url, session)
-        @client = client
-        @url = url
-        @session = session
-      end
-
-      def grades
-        data = fetch['return']['studentDataVOs']
-        courses = {}
-        terms = []
-
-        if data.is_a?(Hash)
-          data = [data]
-        end
-
-        return_data = {}
-
-        if data.nil?
-          puts "===="
-          puts "DATA: #{data}"
-          puts "RES: #{fetch['return']}"
-          puts "===="
-        end
-
-        data.each do |d|
-          final_grades = {}
-
-          d['sections'].each do |sect|
-            if valid_section?(sect)
-              courses[section_title(sect)] = sect['id']
-            end
-          end
-
-          d['reportingTerms'].each do |term|
-            # check if start date has occurred already, and that the end date has *not* occurred already
-            if (Date.parse(term['startDate']) < Date.today) && (Date.parse(term['endDate']) > Date.today)
-              terms << term['id']
-            end
-          end
-
-          next unless d['finalGrades']
-
-          d['finalGrades'].each do |grade|
-            if valid_grade?(grade, courses, terms)
-              final_grades[courses.key(grade['sectionid'])] = grade['percent']
-            end
-          end
-
-          return_data[d['student']['firstName']] = final_grades
-        end
-
-        return_data
-      end
-
-      private
-
-      def fetch
-        student_client = Savon.client(
-          endpoint: @url + "/pearson-rest/services/PublicPortalServiceJSON?response=application/json",
-          namespace: "http://publicportal.rest.powerschool.pearson.com/xsd",
-          digest_auth: ["pearson", "m0bApP5"]
-        )
-
-        transcript_params = {
-          userSessionVO: {
-            userId: @session[:user_id],
-            serviceTicket: @session[:service_ticket],
-            serverInfo: {
-              apiVersion: @session[:server_info][:api_version]
-            },
-            userType: @client.type.to_s
-          },
-          studentIDs: @session[:student_i_ds],
-          qil: {
-            includes: "1"
-          }
-        }
-
-        transcript = student_client.call(:get_student_data, message: transcript_params).to_xml
-        JSON.parse(transcript)
-      end
-
-      def section_title(section)
-        course_name = section['schoolCourseTitle']
-
-        AP_COURSE_WHITELIST[course_name] ||
-          ACC_COURSE_WHITELIST[course_name] ||
-          course_name
-      end
-
-      def valid_grade?(grade, courses, terms)
-        !grade['percent'].nil? &&
-        courses.values.include?(grade['sectionid']) &&
-        terms.include?(grade['reportingTermId']) &&
-        grade['percent'] != 0
-      end
-
-      def valid_section?(section)
-        course_name = section['schoolCourseTitle']
-
-        AP_COURSE_WHITELIST[course_name] ||
-          ACC_COURSE_WHITELIST[course_name] ||
-          ['AP', 'Acc', 'CPA', 'CPB'].any? do |name|
-            course_name.include?(name)
-          end
       end
     end
   end
